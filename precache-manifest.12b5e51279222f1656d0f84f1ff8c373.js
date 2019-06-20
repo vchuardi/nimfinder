9d45b315628e5d28968c93d224893ac876a74547
vchuardi <44575986+vchuardi@users.noreply.github.com>
2019-06-20 14:35:19 +0700

    Updates

---
+++ precache-manifest.12b5e51279222f1656d0f84f1ff8c373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc8b2c01f4c73f60ef1582f51398d9d",
+    "url": "/nimfinder/index.html"
+  },
+  {
+    "revision": "4229cf92a45b435b67af",
+    "url": "/nimfinder/static/css/main.2d1298ea.chunk.css"
+  },
+  {
+    "revision": "863a638ddbb37a8e0d68",
+    "url": "/nimfinder/static/js/2.359672ab.chunk.js"
+  },
+  {
+    "revision": "4229cf92a45b435b67af",
+    "url": "/nimfinder/static/js/main.d412acfc.chunk.js"
+  },
+  {
+    "revision": "f7ca8625f37330da9cda",
+    "url": "/nimfinder/static/js/runtime~main.fea471a8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nimfinder/precache-manifest.12b5e51279222f1656d0f84f1ff8c373.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nimfinder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
